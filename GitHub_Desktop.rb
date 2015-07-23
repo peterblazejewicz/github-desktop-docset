78cb6735b89bdeeb03cf9d149afbf7b3dc77ceef
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-07-23 23:06:30 +0200

    Add first version of cheatsheet file

---
+++ GitHub_Desktop.rb
@@ -0,0 +1,45 @@
+cheatsheet do
+    title 'GitHub Desktop'
+    docset_file_name 'GitHub_Desktop'
+    keyword 'GitHub'
+    source_url 'http://cheat.kapeli.com'
+
+    category do
+        id 'File'
+
+        entry do
+            name 'New Repository'
+            command 'CMD+N'
+        end
+
+        entry do
+            name 'New Branch'
+            command 'CMD+SHIFT+N'
+        end
+
+        entry do
+            name 'New Window'
+            command 'CMD+ALT+N'
+        end
+
+        entry do
+            name 'Clone Repository'
+            command 'CMD+CTRL+O'
+        end
+
+        entry do
+            name 'Add Local Repository'
+            command 'CMD+O'
+        end
+
+        entry do
+            name 'Close'
+            command 'CMD+W'
+        end
+        
+    end
+
+  notes <<-'END'
+    * Created by [Peter Blazejewicz](https://github.com/peterblazejewicz).
+  END
+end
